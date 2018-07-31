4157246a10aed76b617b4fdf1c1b52f0582deaf9
Michael Lötsch <m.loetsch@limesoda.com>
2018-07-31 16:13:36 +0200

    secured api keys

--- src/intents/getgoogleimage.js
+++ src/intents/getgoogleimage.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import myApi from '../../api_keys/googleimageapi';
 
 export const GetGoogleImageHandler = {
     canHandle(handlerInput) {
@@ -7,7 +8,6 @@ export const GetGoogleImageHandler = {
     },
     async handle(handlerInput) {
         let searchQuery = handlerInput.requestEnvelope.request.intent.slots.searchQuery.value;
-        console.log(searchQuery);
         updateCustomSearch(searchQuery);
         let image = await getImage();
         buildResponse(testResponse, image);
@@ -31,46 +31,32 @@ let testResponse = {
     }
 };
 
-//  https://www.googleapis.com/customsearch/v1?key=AIzaSyADdqCpiDM1UEgdq0fcNSxF8fExx5Qhi_E&cx=003811576608300304863:ojbt8aafuuq&defaultToImageSearch=true&q=limesoda
 let customSearch = {
-    apiKey : 'AIzaSyADdqCpiDM1UEgdq0fcNSxF8fExx5Qhi_E',
-    seId : '003811576608300304863:ojbt8aafuuq',
-    searchQuery: 'limesoda',
-    url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyADdqCpiDM1UEgdq0fcNSxF8fExx5Qhi_E&cx=003811576608300304863:ojbt8aafuuq&defaultToImageSearch=true&q=limesoda',
-    // getUrl : function(){
-    //     return `https://www.googleapis.com/customsearch/v1?key=${this.apiKey}&cx=${this.seId}&defaultToImageSearch=true&q=${this.searchQuery}`
-    // }
+    apiKey : myApi.apiKey,
+    seId : myApi.seId,
+    searchQuery: 'chicken',
+    url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyADdqCpiDM1UEgdq0fcNSxF8fExx5Qhi_E&cx=003811576608300304863:ojbt8aafuuq&defaultToImageSearch=true&q=chicken',
+
 };
 
-// let setUrl = (Search) => {
-//     Search.url = `https://www.googleapis.com/customsearch/v1?key=${this.apiKey}&cx=${this.seId}&defaultToImageSearch=true&q=${this.searchQuery}`;
-// };
-
 let updateCustomSearch = (searchQuery) => {
-        // customSearch.searchQuery = searchQuery;
-        // customSearch.getUrl();
         customSearch.url = `https://www.googleapis.com/customsearch/v1?key=${customSearch.apiKey}&cx=${customSearch.seId}&defaultToImageSearch=true&q=${searchQuery}`;
 };
 
 let buildResponse = (response, image) => {
     response.card.image.smallImageUrl = image;
     response.card.image.largeImageUrl = image;
-    console.log(image);
 };
 
 let getImage = async () => {
     try {
-        await console.log(customSearch.url);
         const response = await axios.get(customSearch.url);
         if (response && response.data){
             let imageURL = response.data.items[0].pagemap.cse_image[0].src;
-            console.log(imageURL);
             return imageURL;
         }
     } catch(error) {
         console.error(error);
     }
 };
-
-// getImage();
 
